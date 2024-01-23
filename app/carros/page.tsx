5691b9edf881a36a7f2ad7e00611af920856dcc3
Bruno de Jesus <bruno.jesus.carmo@gmail.com>
2024-01-23 20:17:31 -0300

    .............

--- app/carros/page.tsx
+++ app/carros/page.tsx
@@ -28,13 +28,13 @@ export default function Cars() {
     <div>
       <main className="max-w-7xl mx-auto min-h-screen p-4 mb-72">
         <div className="flex flex-col md:flex-row gap-4">
-          <Card className="w-full md:w-[300px] h-[300px] md:h-[556px] bg-primary rounded-md p-4 mb-4 md:mb-0">
+          <Card className="w-full md:w-[300px] h-auto md:h-[556px] bg-primary rounded-md p-4 mb-4 md:mb-0">
             <h1 className="text-3xl font-bold text-white mb-4">Filtros</h1>
 
             <CarFilterForm onFilterChange={setData} />
           </Card>
 
-          <div className="w-full p-4 flex-1 flex flex-col space-y-7">
+          <div className="w-full p-4 flex-1 flex flex-col space-y-7 md:space-y-0 md:flex-row flex-wrap gap-5">
             {data.map((car) => (
               <Link
                 href={{
@@ -55,32 +55,39 @@ export default function Cars() {
                 }}
                 key={car.id}
               >
-                <div className="w-ful h-56 bg-white flex shadow-md rounded-lg">
-                  <div className="w-[350px] h-full">
-                    <Image
-                      src={car.images[0]}
-                      width={200}
-                      height={200}
-                      alt={car.brandCar}
-                      className="object-cover w-full h-56 rounded-tl-lg rounded-bl-lg"
-                    />
-                  </div>
+                <div className="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 p-2">
+                  <div className="w-full h-auto md:h-96 bg-white flex flex-col shadow-md rounded-lg">
+                    <div className="w-full h-56">
+                      <Image
+                        src={car.images[0]}
+                        width={200}
+                        height={200}
+                        alt={car.brandCar}
+                        className="object-cover w-full h-56 rounded-tl-lg rounded-bl-lg"
+                      />
+                    </div>
 
-                  <div className="p-4 flex flex-col">
-                    <h1 className="text-2xl font-bold text-primary mb-4">
-                      {car.brandCar}{" "}
-                      <span className="text-black">{car.modelCar}</span>
-                    </h1>
+                    <div className="p-4 flex flex-col flex-grow">
+                      <h1 className="text-xl md:text-2xl lg:text-3xl font-bold text-primary mb-2">
+                        {car.brandCar}{" "}
+                        <span className="text-black">{car.modelCar}</span>
+                      </h1>
 
-                    <p className="font-black">Ano: {car.yearFabrication}</p>
-                    <p className="font-black">KM: {car.km}</p>
-                    <p className="font-black">
-                      Tipo de combustível: {car.fuel}
-                    </p>
-                    <p className="font-black">Localização: {car.location}</p>
-                    <p className="font-bold text-2xl mt-2 text-primary">
-                      Valor: {car.price}
-                    </p>
+                      <p className="font-black mb-2">
+                        Ano: {car.yearFabrication}
+                      </p>
+                      <p className="font-black mb-2">KM: {car.km}</p>
+                      <p className="font-black mb-2">
+                        Tipo de combustível: {car.fuel}
+                      </p>
+                      <p className="font-black mb-2">
+                        Localização: {car.location}
+                      </p>
+
+                      <p className="font-bold text-lg md:text-2xl text-primary mt-2">
+                        Valor: {car.price}
+                      </p>
+                    </div>
                   </div>
                 </div>
               </Link>
